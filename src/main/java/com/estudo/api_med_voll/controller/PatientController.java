74d49190cc32c53734454be7f6f5d57123a7d325
harrissondutra <harrissondutra@gmail.com>
2024-08-20 20:50:41 -0300

    Ajuste no Swagger

--- src/main/java/com/estudo/api_med_voll/controller/PatientController.java
+++ src/main/java/com/estudo/api_med_voll/controller/PatientController.java
@@ -18,7 +18,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
-@Tag(name = "Gerenciar Pacientes", description = "Operações relacionadas a médicos")
+@Tag(name = "Gerenciar Pacientes", description = "Operações relacionadas a pacientes")
 @RestController
 @RequestMapping("/patient")
 public class PatientController {
